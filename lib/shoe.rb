2082718d57d88bcfafe5a213d35895e653f3108f
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-10 16:40:21 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe 
+  attr_accessor :color, :size, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    
+  end
+  
+end
+
